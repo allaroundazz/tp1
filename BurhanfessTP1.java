86ce5696485379c6a0eefb35b40cd3c4cb637491
MacBook Air <macbookair@diazs-lappy.local>
2025-06-20 15:59:10 +0700

    SecondCommit

---
+++ BurhanfessTP1.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class BurhanfessTP1 {
+    public static void main(String[] args) {
+        //Mencetak bagian Ascii art sebagai pembuka program
+        System.out.print("/------------------------------------------------------------------------------------------\\\n" + //
+                        "| ________                    ___                              __                          |\n" + //
+                        "| `MMMMMMMb.                  `MM                             69MM                         |\n" + //
+                        "|  MM    `Mb                   MM                            6M' `                         |\n" + //
+                        "|  MM     MM ___   ___ ___  __ MM  __      ___    ___  __   _MM__ ____     ____     ____   |\n" + //
+                        "|  MM    .M9 `MM    MM `MM 6MM MM 6MMb   6MMMMb   `MM 6MMb  MMMMM6MMMMb   6MMMMb\\  6MMMMb\\ |\n" + //
+                        "|  MMMMMMM(   MM    MM  MM69 \" MMM9 `Mb 8M'  `Mb   MMM9 `Mb  MM 6M'  `Mb MM'    ` MM'    ` |\n" + //
+                        "|  MM    `Mb  MM    MM  MM'    MM'   MM     ,oMM   MM'   MM  MM MM    MM YM.      YM.      |\n" + //
+                        "|  MM     MM  MM    MM  MM     MM    MM ,6MM9'MM   MM    MM  MM MMMMMMMM  YMMMMb   YMMMMb  |\n" + //
+                        "|  MM     MM  MM    MM  MM     MM    MM MM'   MM   MM    MM  MM MM            `Mb      `Mb |\n" + //
+                        "|  MM    .M9  YM.   MM  MM     MM    MM MM.  ,MM   MM    MM  MM YM            ,MM      ,MM |\n" + //
+                        "| _MMMMMMM9'   YMMM9MM__MM_   _MM_  _MM_`YMMM9'Yb._MM_  _MM__MM_ YMMMM9  MYMMMM9  MYMMMM9  |\n" + //
+                        "\\------------------------------------------------------------------------------------------/\n\n ");
+        //*********************************************************************************************************************************
+        Scanner minta = new Scanner(System.in);
+        int codevibe = 0;
+
+        System.out.println("Pilih Mode (0 == if-else, 1 = switch-mode)");
+        int mode = minta.nextInt();
+
+        //input 1
+        System.out.println("Ketik 1 kalau kamu tipe orang yang tidak ingin diketahui orang lain saat mengirim menfess ");
+        int input1 = minta.nextInt();
+        if (input1 == 1) {codevibe+=1;}
+
+        //input 2
+        System.out.println("Apakah kamu tipe orang yang suka pakai kode saat ingin menyanmpaikan perasaan? ");
+        String input2 = minta.next();
+        if (input2.equalsIgnoreCase("ya")) {
+            codevibe += 2;}
+
+        //input 3
+        System.out.println("Ketik 1 kalau kamu suka kirim menfess yang ambigu agar menimbulkan rasa penasaran.");
+        int input3 = minta.nextInt();
+        if (input3 == 1) {codevibe += 4;} 
+
+        //input 4
+        System.out.println("Apakah kamu tipe orang yang aktif mengirim manfess penuh emosi sampi menjadi bahan obrolan? (ya) (tidak)");
+        String input4 = minta.next();
+        if (input4.equalsIgnoreCase("ya")) {codevibe += 8;} 
+
+        //input 5 
+        System.out.println("Ketik satu kalau kamu jarang kirim menfess, tapi sekalinya membuat langsung bikin ramai.");
+        int input5 = minta.nextInt();
+        if (input5 == 1) {codevibe += 16;}
+        
+        //mencetak kriteria user berdasarkan kodevibenya
+        System.out.println("Total kodevibe kamu adalah " + codevibe + " jadi: ");
+        if (mode == 0) {
+            if (codevibe <= 5) {
+                System.out.println("\n    Kamu tipe 'pengagum diam-diam'. MenFess-mu jarang, tapi kalau muncul bikin kaget.\n");
+            } if (codevibe > 5 && codevibe <= 10) {
+                System.out.println("\n    Kamu tibe 'semi-aktif'. Kadang muncul dengan kode, kadang ngilang.\n");
+            } if (codevibe > 10 && codevibe <= 15) {
+                System.out.println("\n    Kamu tipe 'suka bikin penasaran'. MenFess-mu bikin orang mikir, tapi kadang bikin bingung.\n");
+            } if (codevibe > 15 && codevibe <= 20) {
+                System.out.println("\n    Kamu tibe 'suka bikin drama'. MenFess-mu bikin orang penasaran, tapi terlalu banyak drama.\n");
+            } if (codevibe > 20 && codevibe <= 25) {
+                System.out.println("\n    Kamu tipe 'suka bikin orang mikir'.MenFess-mu bikin orang penasaran, tapi kadang bikin mereka mikir\n");
+            } if (codevibe > 25 && codevibe <= 31) {
+                System.out.println("\n    Kamu tipe 'rahasia'. MenFess-mu jarang muncul, tapi kalau muncul bikin orang penasaran siapa yang ngirim.\n");
+            }
+        }
+        //*********************************************************************************************************************************
+         if (mode == 1) {
+            switch (codevibe/6) {
+                case 0:
+                    System.out.println("\n    Kamu tipe 'pengagum diam-diam'. MenFess-mu jarang, tapi kalau muncul bikin kaget. : via Switch-Case\n");
+                    break;
+                case 1:
+                    System.out.println("\n    Kamu tibe 'semi-aktif'. Kadang muncul dengan kode, kadang ngilang. : via Switch-Case\n");
+                    break;
+                case 2:
+                    System.out.println("\n    Kamu tipe 'suka bikin penasaran'. MenFess-mu bikin orang mikir, tapi kadang bikin bingung. : via Switch-Case\n");
+                    break;
+                case 3:
+                    System.out.println("\n    Kamu tibe 'suka bikin drama'. MenFess-mu bikin orang penasaran, tapi terlalu banyak drama. : via Switch-Case\n");
+                    break;
+                case 4:
+                    System.out.println("\n    Kamu tipe 'suka bikin orang mikir'.MenFess-mu bikin orang penasaran, tapi kadang bikin mereka mikir : via Switch-Case\n");
+                    break;
+                case 5:
+                    System.out.println("\n    Kamu tipe 'rahasia'. MenFess-mu jarang muncul, tapi kalau muncul bikin orang penasaran siapa yang ngirim. : via Switch-Case\n");
+                    break;
+                }
+                
+            }
+
+         }
+
+    }
+
+
