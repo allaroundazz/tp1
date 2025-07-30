49bcffbf8b14a4c311140ec4477e900959f5e417
Diaz Prayodhi <diazprayodhi53@gmail.com>
2025-07-30 15:18:12 +0700

    Create ListMatkul.java

---
+++ ListMatkul.java
@@ -0,0 +1,55 @@
+Semester 1:
+
+Matematika Dasar I | Senin | 08:00 - 09:40
+Pengantar Teknologi Informasi | Selasa | 10:00 - 11:40
+Algoritma dan Pemrograman I | Rabu | 13:00 - 14:40
+Bahasa Inggris I | Kamis | 15:00 - 16:40
+
+Semester 2:
+
+Matematika Dasar II | Senin | 08:00 - 09:40
+Struktur Data | Selasa | 10:00 - 11:40
+Algoritma dan Pemrograman II | Rabu | 13:00 - 14:40
+Fisika Dasar | Kamis | 15:00 - 16:40
+
+Semester 3:
+
+Matematika Diskrit | Senin | 08:00 - 09:40
+Basis Data | Selasa | 10:00 - 11:40
+Pemrograman Berorientasi Objek | Rabu | 13:00 - 14:40
+Sistem Operasi | Kamis | 15:00 - 16:40
+
+Semester 4:
+
+Analisis dan Desain Algoritma | Senin | 08:00 - 09:40
+Jaringan Komputer | Selasa | 10:00 - 11:40
+Rekayasa Perangkat Lunak | Rabu | 13:00 - 14:40
+Statistika dan Probabilitas | Kamis | 15:00 - 16:40
+
+Semester 5:
+
+Kecerdasan Buatan | Senin | 08:00 - 09:40
+Pemrograman Web | Selasa | 10:00 - 11:40
+Keamanan Sistem Informasi | Rabu | 13:00 - 14:40
+Manajemen Proyek TI | Kamis | 15:00 - 16:40
+
+Semester 6:
+
+Machine Learning | Senin | 08:00 - 09:40
+Pemrograman Mobile | Selasa | 10:00 - 11:40
+Sistem Terdistribusi | Rabu | 13:00 - 14:40
+Interaksi Manusia dan Komputer | Kamis | 15:00 - 16:40
+
+Semester 7:
+
+Data Mining | Senin | 08:00 - 09:40
+Cloud Computing | Selasa | 10:00 - 11:40
+Etika Profesi | Rabu | 13:00 - 14:40
+Metodologi Penelitian | Kamis | 15:00 - 16:40
+
+Semester 8:
+
+Tugas Akhir | Senin | 08:00 - 09:40
+Seminar | Selasa | 10:00 - 11:40
+Kerja Praktik | Rabu | 13:00 - 14:40
+Technopreneurship | Kamis | 15:00 - 16:40
